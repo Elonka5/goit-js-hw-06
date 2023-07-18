e481d4900ef1ccb546e778c8fb0edf6cf0fa9936
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-07-18 16:10:25 +0300

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -2,12 +2,12 @@ const form = document.querySelector(".login-form");
 
 const onhandlerSubmit = event => {
   event.preventDefault();
-  const { email, password } = event.currentTarget.elements;
-  if (email.value === '' || password.value === '') {
+  const { email, password } = event.target.elements;
+  if (!email.value || !password.value) {
     return alert("Please fill in all the fields!");
   }
   console.log(`Email: ${email.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
+  event.target.reset();
 };
 
 form.addEventListener("submit", onhandlerSubmit);
